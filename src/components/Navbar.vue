58ab65c40cf1d51aeab63a26ae712a37014f9a61
Kristijan-Kekic <kkekic@unipu.hr>
2021-02-06 16:38:32 +0100

    Dodani novi fajlovi

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div id="nav" class="navbar navbar-light">
+      <router-link to="/kosarica">Košarica</router-link> |
+      <router-link to="/">Naslovnica</router-link>
+      <router-link to="/login-reg">Prijava/registracija</router-link> |
+      </div>
+    <router-view/>
+  </div>
+ </template>
+
+ <script>
+export default {
+  name: 'Navbar',
+};
+</script>
